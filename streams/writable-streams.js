c144a0b30c202745a19be79682b9b73e226fa8b0
rjfm <javier.fuentesm@hotmail.com>
2019-10-16 11:10:40 -0500

    wirtable and readable streams

---
+++ streams/writable-streams.js
@@ -0,0 +1,8 @@
+const {Writable} = require('stream');
+const writableStream = new Writable({
+    write(chunk,encoding,callback){
+        console.log(chunk.toString())
+        callback()
+    }
+})
+process.stdin.pipe(writableStream)
