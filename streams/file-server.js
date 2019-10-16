e7d0c7baa7bf6611c061ce387f1b122712114194
rjfm <javier.fuentesm@hotmail.com>
2019-10-16 10:38:33 -0500

    Se crean streams

---
+++ streams/file-server.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const server = require("http").createServer();
+server.on("request", (req, res) => {
+  fs.readFile("./big", (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+    res.end(data);
+  });
+});
+
+server.listen(3000);
