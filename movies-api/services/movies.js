9956d9a47e4f20e001ab79e97c66174b65e07262
rjfm <javier.fuentesm@hotmail.com>
2019-10-22 17:47:32 -0500

    Error con mongo

--- movies-api/services/movies.js
+++ movies-api/services/movies.js
@@ -1,31 +1,39 @@
-const { moviesMock } = require('../utils/mocks/movies');
-
+const MongoLib = require('../lib/mongo');
 class MoviesService {
-  async getMovies() {
-    const movies = await Promise.resolve(moviesMock);
+  constructor() {
+    this.colection = 'movies';
+    this.mongoDB = new MongoLib();
+  }
+  async getMovies({ tags }) {
+    const query = tags && { tags: { $in: tags } };
+    const movies = await this.mongoDB.getAll(this.collection, query);
     return movies || [];
   }
-  async getMovie() {
-    const movie = await Promise.resolve(moviesMock[0]);
+  async getMovie({ movieId }) {
+    const movie = await this.mongoDB.get(this.collection, movieId);
     return movie || {};
   }
-  async createMovie() {
-    const createdMovieId = await Promise.resolve(moviesMock[0].id);
+  async createMovie({ movie }) {
+    const createdMovieId = await this.mongoDB.create(this.collection, movie);
     return createdMovieId;
   }
 
-  async updateMovie() {
-    const updatedMovieId = await Promise.resolve(moviesMock[0].id);
+  async updateMovie({ movieId, movie } = {}) {
+    const updatedMovieId = await this.mongoDB.update(
+      this.collection,
+      movieId,
+      movie
+    );
     return updatedMovieId;
   }
-  async deleteMovie() {
-    const deletedMovieId = await Promise.resolve(moviesMock[0].id);
+  async deleteMovie({ movieId }) {
+    const deletedMovieId = await this.mongoDB.delete(this.collection, movieId);
     return deletedMovieId;
   }
-  async patchMovie() {
-    const patchedMovieId = await Promise.resolve(moviesMock[0].id);
-    return patchedMovieId;
-  }
+  //async patchMovie() {
+  // const patchedMovieId = await Promise.resolve(moviesMock[0].id);
+  //return patchedMovieId;
+  // }
 }
 
 module.exports = MoviesService;
