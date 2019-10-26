c41978f7f7a073aa98b1bee3aa9ac09f5da46bf4
rjfm <javier.fuentesm@hotmail.com>
2019-10-25 20:16:07 -0500

    Nuevo error con mocha

--- movies-api/utils/mocks/movies.js
+++ movies-api/utils/mocks/movies.js
@@ -81,6 +81,22 @@ const moviesMock = [
   }
 ];
 
+function filteredMoviesMock(tag) {
+  return moviesMock.filter(movie => movie.tags.includes(tag));
+}
+
+class MovieServiceMock {
+  async getMovies() {
+    return Promise.resolve(moviesMock);
+  }
+
+  async createMovies() {
+    return Promise.resolve(moviesMock[0]);
+  }
+}
+
 module.exports = {
-  moviesMock
+  moviesMock,
+  filteredMoviesMock,
+  MovieServiceMock
 };
