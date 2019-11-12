11501c485c26a47ba851e76457c75040aba328b2
rjfm <javier.fuentesm@hotmail.com>
2019-11-12 09:14:31 -0600

    Se implementa atenticacion con google

--- ssr-server/index.js
+++ ssr-server/index.js
@@ -14,6 +14,9 @@ app.use(cookieParser());
 
 //  Basic strategy
 require("./utils/auth/strategies/basic");
+//OAuth strategy
+
+require("./utils/auth/strategies/oauth");
 
 app.post("/auth/sign-in", async function(req, res, next) {
   passport.authenticate("basic", function(error, data) {
@@ -102,6 +105,27 @@ app.delete("/user-movies/:userMovieId", async function(req, res, next) {
     next(error);
   }
 });
+app.get(
+  "/auth/google-oauth",
+  passport.authenticate("google-oauth", {
+    scope: ["email", "profile", "openid"]
+  })
+);
+app.get(
+  "/auth/google-oauth",
+  passport.authenticate("google-oauth", { session: false }),
+  (req, res, next) => {
+    if (!req.user) {
+      next(boom.unauthorized());
+    }
+    const { token, ...user } = req.user;
+    res.cookie("token", token, {
+      httpOnly: !config.dev,
+      secure: !config.dev
+    });
+    res.status(200).json(user);
+  }
+);
 
 app.listen(config.port, function() {
   console.log(`Listening http://localhost:${config.port}`);
